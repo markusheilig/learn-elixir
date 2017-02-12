c680c80649d34a7516db22a66b6af023d91c28b7
Markus Heilig <markusheilig@gmx.net>
2017-02-12 19:37:47 +0100

    add chapter6 solution

---
+++ chapter6_function_calls_and_pattern_matching.exs
@@ -0,0 +1,21 @@
+# ModulesAndFunctions-1, ModulesAndFunctions-3
+defmodule Times do
+
+  def double(n) do
+    n * 2
+  end
+
+  def triple(n) do
+    n * 3
+  end
+
+  def quadruple(n) do
+    double(n) + double(n)
+  end
+
+end
+
+# ModulesAndFunctions-2
+# iex chapter6_function_calls_and_pattern_matching.exs
+# or
+# iex> c "chapter6_function_calls_and_pattern_matching.exs"
