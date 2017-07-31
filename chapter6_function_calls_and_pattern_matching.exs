e7dd4329d66e40c53c4dea4be7c7c4cbbf1d8e3f
Markus Heilig <markusheilig@gmx.net>
2017-07-31 10:12:04 +0200

    add ex4 and ex5 for chapter 6

--- chapter6_function_calls_and_pattern_matching.exs
+++ chapter6_function_calls_and_pattern_matching.exs
@@ -19,3 +19,19 @@ end
 # iex chapter6_function_calls_and_pattern_matching.exs
 # or
 # iex> c "chapter6_function_calls_and_pattern_matching.exs"
+
+
+defmodule MyModule do
+
+  # ModulesAndFunctions-4
+  def sum(0), do: 0
+  def sum(n), do: n + sum(n-1)
+
+  # ModulesAndFunctions-5
+  def gcd(x, 0), do: x
+  def gcd(x, y), do: gcd(y, rem(x, y))
+
+end
+
+IO.puts "Sum from 1 to 10 is #{MyModule.sum(10)}"
+IO.puts "Greatest Common Divisor of 45 and 105 is #{MyModule.gcd(45, 105)}"
