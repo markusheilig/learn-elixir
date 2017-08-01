c9ad21a3a10c0fadf30505cac7d7f11b4044328c
Markus Heilig <markusheilig@gmx.net>
2017-08-01 18:53:31 +0200

    update caesar encryption for lowercase and uppercase characters

--- chapter7.exs
+++ chapter7.exs
@@ -21,10 +21,16 @@ defmodule MyList do
     # ListsAndRecursion-3
     def caesar(list, n), do: _caesar(list, n, [])
     defp _caesar([], _, encrypted), do: encrypted        
-    defp _caesar([head | tail], n, encrypted) do
-        encrypted_char = rem(head + n - 97, 26) + 97        
-        _caesar(tail, n, encrypted ++ [encrypted_char])
+    defp _caesar([head | tail], n, encrypted) do        
+        _caesar(tail, n, encrypted ++ [encrypt(head, n)])
     end    
+    defp encrypt(char, n) when char >= 97 and char <= 122 do
+        rem(char + n - 97, 26) + 97
+    end
+    defp encrypt(char, n) when char >= 65 and char <= 90 do
+        rem(char + n - 65, 26) + 65
+    end    
+    defp encrypt(_char, _n), do: '?'
 
 end
 
@@ -37,4 +43,4 @@ IO.puts "mapsum is #{mapsum}"
 max = MyList.max [18, 27, 89, 31, 101, 12]
 IO.puts "max is #{max}"
 
-IO.puts MyList.caesar('ryvkve', 13)
+IO.puts MyList.caesar('Ryvkve', 13)
